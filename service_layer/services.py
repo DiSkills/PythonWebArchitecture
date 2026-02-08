44b39cbcaca7731ccb72bc51bf7d0648c8bedd6c
DiSkills <github@diskills-developer.org>
2026-02-08 17:22:10 +0300

    added the service layer

---
+++ service_layer/services.py
@@ -0,0 +1,19 @@
+from adapters.repository import AbstractRepository
+from domain import model
+
+
+class InvalidSku(Exception):
+    pass
+
+
+def is_valid_sku(sku: str, batches: list[model.Batch]) -> bool:
+    return sku in {b.sku for b in batches}
+
+
+def allocate(line: model.OrderLine, repo: AbstractRepository, session) -> str:
+    batches = repo.list()
+    if not is_valid_sku(line.sku, batches):
+        raise InvalidSku(f"Invalid sku {line.sku}")
+    batchref = model.allocate(line, batches)
+    session.commit()
+    return batchref
